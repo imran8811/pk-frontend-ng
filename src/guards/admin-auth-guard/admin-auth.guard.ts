4c5f87184bcee420548d05651c85f658e8ddbdd5
Mohd Imran <m.imran8811@gmail.com>
2023-07-28 17:14:19 +0500

    changes

--- src/guards/admin-auth-guard/admin-auth.guard.ts
+++ src/guards/admin-auth-guard/admin-auth.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AdminAuthService } from 'src/services'
 
 @Injectable({ providedIn: 'root' })
 
-export class AdminAuthGuard implements CanActivate {
+export class AdminAuthGuard  {
 
   constructor(private adminAuthService : AdminAuthService, private router: Router){}
 
